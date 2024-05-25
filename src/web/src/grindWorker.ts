9d038e2e700d8ac83a7689517cbc7b996294b83b
juzybits <96890594+juzybits@users.noreply.github.com>
2024-05-25 13:00:51 +0000

    new Ed25519Keypair() instead of mnemonic (much faster)

--- src/web/src/grindWorker.ts
+++ src/web/src/grindWorker.ts
@@ -1,8 +1,6 @@
 import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
-import { generateMnemonic } from "@scure/bip39";
-import { wordlist } from "@scure/bip39/wordlists/english";
 
-self.onmessage = (evt: MessageEvent<string>) => {
+self.onmessage = (evt: MessageEvent<unknown>) => {
     const msg = evt.data;
     if (msg === "start") {
         grind();
@@ -10,19 +8,16 @@ self.onmessage = (evt: MessageEvent<string>) => {
 };
 
 const grind = () => {
-    // let count = 0;
+    let count = 0;
     while (true) {
-        const phrase = generateMnemonic(wordlist);
-        const pair = Ed25519Keypair.deriveKeypair(phrase);
+        const pair = new Ed25519Keypair();
         const address = pair.toSuiAddress();
         if (address.startsWith("0xfee")) {
             const secretKey = pair.getSecretKey();
-            postMessage({address, secretKey, phrase});
+            postMessage({address, secretKey});
         }
-        // if (++count % 100 === 0) {
-        //     console.log(count);
-        // }
+        if (++count % 1000 === 0) {
+            console.log(count);
+        }
     }
 };
-
-// const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
