b197af326148266e68991f2e9d0b52fb4c81bdb9
juzybits <96890594+juzybits@users.noreply.github.com>
2024-05-25 15:45:31 +0000

    pair count

--- src/web/src/PageHome.tsx
+++ src/web/src/PageHome.tsx
@@ -10,9 +10,10 @@ export const PageHome: React.FC = () =>
 
     const worker = useRef<Worker|null>(null);
     const [ status, setStatus ] = useState<WorkerStatus>("stopped");
-    const [ startsWith, setStartsWith ] = useState<string>("ab");
+    const [ startsWith, setStartsWith ] = useState<string>("abc");
     const [ endsWith, setEndsWith ] = useState<string>("");
     const [ keypairs, setKeypairs ] = useState<Keypair[]>([]);
+    const [ pairCount, setPairCount ] = useState<number>(0);
 
     /* Functions */
 
@@ -26,7 +27,10 @@ export const PageHome: React.FC = () =>
     const handleWorkerEvent = (evt: MessageEvent<WorkerEvent>) => {
         const e = evt.data;
         if (e.msg === "match") {
-            setKeypairs(oldMatches => [e.data.pair, ...oldMatches]);
+            setKeypairs(oldMatches => [e.pair, ...oldMatches]);
+        }
+        else if (e.msg === "countUpdate") {
+            setPairCount(oldCount => oldCount + e.count);
         }
         else if (e.msg === "restart") {
             stopWorker();
@@ -49,10 +53,7 @@ export const PageHome: React.FC = () =>
         );
         worker.current.onmessage = handleWorkerEvent;
 
-        const event: AppStartEvent = {
-            msg: "start",
-            data: { startsWith, endsWith },
-        };
+        const event: AppStartEvent = { msg: "start", startsWith, endsWith };
         worker.current.postMessage(event);
     };
 
@@ -81,7 +82,10 @@ export const PageHome: React.FC = () =>
         }
     };
 
-    const criteriaLength = startsWith.length + endsWith.length;
+    const startsLength = startsWith.length;
+    const endsLength = endsWith.length;
+    const criteriaLength = startsLength + endsLength;
+
     const searchSpace = Math.pow(16, criteriaLength);
     const searchSpaceStr = searchSpace < 1_000
         ? String(searchSpace)
@@ -90,39 +94,58 @@ export const PageHome: React.FC = () =>
     /* HTML */
 
     return <>
+
     <div id="config">
         <p>Begins with:</p>
         <input type="text" value={startsWith} onChange={onChangeStartsWith} />
         <p>Ends with:</p>
         <input type="text" value={endsWith} onChange={onChangeEndsWith} />
     </div>
+
     <div className="btn-group">
         <button className="btn" onClick={startWorker} disabled={status !== "stopped"}>Start</button>
         <button className="btn" onClick={stopWorker} disabled={status !== "running"}>Stop</button>
     </div>
+
     {criteriaLength > 0 &&
     <p>
         Search space: {searchSpaceStr}
     </p>
     }
+
+    {pairCount > 0 &&
+    <p>
+        Pairs generated: {formatNumber(pairCount, "compact")}
+    </p>
+    }
+
     {keypairs.length > 0 &&
     <div id="matches">
         <h2>Matches</h2>
         {keypairs.map(pair =>
         <div className="match" key={pair.address}>
-            <div className="short-address">{shortenSuiAddress(
-                pair.address,
-                Math.max(4, startsWith.length),
-                Math.max(4, endsWith.length),
-                undefined,
-                "",
-            )}</div>
+            <div className="short-address">{shortAddress(pair.address, startsLength, endsLength)}</div>
             <div className="address">{pair.address}</div>
             <div className="secret-key">{pair.secretKey}</div>
         </div>)}
     </div>
     }
+
     </>;
 };
 
 const isHex = (str: string) => /^[0-9a-fA-F]*$/.test(str);
+
+const shortAddress = (
+    address: string,
+    startsLength: number,
+    endsLength: number,
+): string => {
+    return shortenSuiAddress(
+        address,
+        Math.max(4, startsLength),
+        Math.max(4, endsLength),
+        undefined,
+        "",
+    );
+}
--- src/web/src/vanityWorker.ts
+++ src/web/src/vanityWorker.ts
@@ -1,6 +1,7 @@
 import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
 
 const RESTART_AFTER_COUNT = 5000;
+const UPDATE_AFTER_COUNT = 100;
 
 export type Keypair = {
     address: string;
@@ -10,27 +11,27 @@ export type Keypair = {
 export type AppEvent = AppStartEvent;
 export type AppStartEvent = {
     msg: "start";
-    data: {
-        startsWith: string;
-        endsWith: string;
-    };
+    startsWith: string;
+    endsWith: string;
 };
 
-export type WorkerEvent = WorkerMatchEvent | WorkerRestartEvent;
+export type WorkerEvent = WorkerMatchEvent | WorkerRestartEvent | WorkerCountUpdateEvent;
 export type WorkerMatchEvent = {
     msg: "match";
-    data: {
-        pair: Keypair;
-    };
+    pair: Keypair;
 };
 export type WorkerRestartEvent = {
     msg: "restart";
+};
+export type WorkerCountUpdateEvent = {
+    msg: "countUpdate";
+    count: number;
 };
 
 self.onmessage = (evt: MessageEvent<AppEvent>) => {
     const e = evt.data;
     if (e.msg === "start") {
-        run("0x" + e.data.startsWith, e.data.endsWith);
+        run("0x" + e.startsWith, e.endsWith);
     }
 };
 
@@ -46,18 +47,19 @@ const run = (startsWith: string, endsWith: string) => {
             const secretKey = pair.getSecretKey();
             const event: WorkerMatchEvent = {
                 msg: "match",
-                data: { pair: { address, secretKey } },
+                pair: { address, secretKey },
             };
             postMessage(event);
         }
 
-        if (count % 1000 === 0) {
-            console.debug(count);
+        if (count % UPDATE_AFTER_COUNT === 0) {
+            const event: WorkerCountUpdateEvent = { msg: "countUpdate", count: UPDATE_AFTER_COUNT };
+            postMessage(event);
         }
 
         if (count === RESTART_AFTER_COUNT) {
             console.debug("[worker] restarting");
-            const event: WorkerEvent = { msg: "restart" };
+            const event: WorkerRestartEvent = { msg: "restart" };
             postMessage(event);
             break;
         }
